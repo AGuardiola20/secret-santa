9f79e8cf6bb66c7a3e16ecf3406339a4746b01a6
AGuardiola20 <andres.pguardiola@gmail.com>
2024-12-03 13:13:05 -0600

    add: NewCodeScreen

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import SplashScreen from "./screens/SplashScreen";
 import CodeScreen from "./screens/CodeScreen";
+import NewCodeScreen from "./screens/NewCodeScreen";
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <Routes>
         <Route path="/" element={<SplashScreen />} />
         <Route path="/code" element={<CodeScreen />} />
+        <Route path="/newCode" element={<NewCodeScreen />} />
       </Routes>
     </Router>
   );
---
+++ src/screens/NewCodeScreen.tsx
@@ -0,0 +1,72 @@
+import styled from "styled-components";
+import Gifts from "../assets/imgs/Gifts.svg";
+import Btn from "../components/Btn";
+import { useNavigate } from "react-router-dom";
+
+const Container = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-color: ${({ theme }) => theme.colors.primary};
+  height: 100vh;
+  text-align: center;
+  padding: 0 20px;
+`;
+
+const Main = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  gap: 2rem;
+  max-width: 500px;
+  width: 100%;
+`;
+
+const Image = styled.img`
+  width: 200px;
+  height: 200px;
+  border-radius: 50%;
+  background-color: ${({ theme }) => theme.colors.background};
+  box-shadow: 0px 4px 20px rgba(0, 0, 0, 0.15);
+`;
+
+const Title = styled.h1`
+  font-size: 2.7rem;
+  font-weight: 600;
+  color: ${({ theme }) => theme.colors.textSecondary};
+`;
+
+const Text = styled.p`
+  color: ${({ theme }) => theme.colors.textSecondary};
+  font-size: 1.3rem;
+  font-weight: 300;
+`;
+
+const Line = styled.div`
+  width: 20%;
+  border-bottom: 2px solid ${({ theme }) => theme.colors.accent};
+  margin: 10px 0;
+`;
+
+const NewCodeScreen = () => {
+  const navigate = useNavigate();
+
+  return (
+    <Container>
+      <Main>
+        <Image src={Gifts} alt="Gifts image" />
+        <Title>¡Ups! Pantalla en desarrollo...</Title>
+        <Text>No se le pagó lo suficiente a Andres 😅</Text>
+        <Line />
+        <Btn
+          text="Volver"
+          isPrimary={false}
+          onClick={() => navigate("/code")}
+        />
+      </Main>
+    </Container>
+  );
+};
+
+export default NewCodeScreen;
